33e3e5f7336820416a5a6ee1ffdf2e9f13dc852b
elisa33 <pipi.tdfuego@gmail.com>
2021-06-03 12:09:18 -0400

    new javascrip portfolio

---
+++ js/cards.js
@@ -0,0 +1,130 @@
+window.addEventListener('load', loadData);
+
+function loadData() {
+    fetch('cards.json')
+        .then((respuesta) => respuesta.json())
+        .then((data) => {
+            showData(data);
+        });
+}
+
+    function showData(data) {
+        
+        const { projects } = data;
+        const portfolio = document.querySelector('.portfolio'); 
+       
+        projects.forEach((element, index) => {
+
+            /* colocamos el titulo */
+            const title = document.createElement('h2')
+            title.innerText = (Object.keys(projects[index])[0]);
+            portfolio.appendChild(title)
+            
+            /* creamos el contenedor de las cards */
+            const cards = document.createElement('div');
+            cards.classList.add('cards');
+            portfolio.appendChild(cards)
+           
+            /* Iteramos al objeto */
+            let claves = Object.keys(element); 
+
+            for(let i=0; i< claves.length; i++){
+
+                const array = element[claves[i]];
+                for (let i of array.keys()){
+                    //console.log(array[i])
+                    const { id,  
+                        client, 
+                        icon01, 
+                        icon02,
+                        img,
+                        clase,
+                        rol,
+                        techs,
+                        mode } = array[i];
+        
+        
+                        const card = document.createElement('div');
+                
+                        const contenido = ` 
+                                <div id="${id}">
+                                    <div class="contain">
+                                        <img src="${img}" alt="" />
+                                    </div>
+                                    <div class="flex">
+                                        <a href="#dm">${client}</a>
+                                        <div class="tech">
+                                            ${icon01}
+                                            ${icon02}
+                                        </div>
+                                    </div>
+                                </div> 
+                            `;
+        
+                    card.classList.add('card');
+                    card.classList.add(`${clase}`);
+                    card.innerHTML = contenido;
+                    cards.appendChild(card);
+        
+                    card.addEventListener('click', showModal);
+        
+                    //Modals 
+            
+                    function showModal(){
+                        
+                        
+                        const overlay = document.querySelector('.overlay');
+                        overlay.classList.add('active');
+                        const modal = document.createElement('div');
+                        modal.classList.add('modal')
+                        
+                            const modalContent = `
+                                        <a href="" id="btn-close" class="btn-close">X</a>
+                                        <div class="m-info">
+                                            <img src="${img}" alt="" />
+                                            <div class="project">
+                                                <div class="pr-info">
+                                                    <h3>Client:</h3>
+                                                    <p>${client}</p>
+                                                </div>
+                                                <div class="pr-info">
+                                                    <h3>Rol:</h3>
+                                                    <p>${rol}</p>
+                                                </div>
+                                                <div class="pr-info">
+                                                    <h3>Techs:</h3>
+                                                    <p>${techs}</p>
+                                                </div>
+                                                <div class="pr-info">
+                                                    <h3>Mode:</h3>
+                                                    <p>${mode}</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                `
+                            modal.innerHTML = modalContent;
+                            overlay.appendChild(modal);
+                
+                        };
+                                        
+                } 
+           
+            }
+
+
+            /* const keys = Object.keys(element);
+            console.log(keys);
+            console.log(keys[index]);
+            const array = element.keys;  */
+       
+        })
+    }
+
+    
+      
+    
+        
+    
+
+   
+
